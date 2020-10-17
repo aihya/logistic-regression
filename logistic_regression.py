fixing gradient descent

logistic_regression.py
@@ -18,9 +18,11 @@ class LogisticRegression():
         cost = -(1/m) * np.sum((y * np.log(hoX)) + ((1 - y)*np.log(1 - hoX)))
         return cost + reg_term
 
-    def gradient(self, X, weights, y, _alpha, _lambda, iters):
+    def gradient_descent(self, X, weights, y, _alpha, _lambda, iters):
         m = y.length
         for _ in range(iters):
-            hoX = h(weights, X)
-            dJ = (1/m) * np.matmul(X.T, hoX) + ((_lambda/m) * weights)
-            weights = weights - (_alpha/m) * np.matmul(X.T, )
+            hoX = self.h(weights, X)
+            dJ = (1/m) * np.matmul(X.T, hoX - y)
+            dJ[1:] = dJ[1:] + (_lambda/m)*weights[1:]
+            weights = weights - (_alpha * dJ)
+    