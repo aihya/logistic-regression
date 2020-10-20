6b699d36b855a9b314827bc6cc2c734384fdf290
aihya <aihya@student.1337.ma>
2020-10-20 14:03:46 +0100

    new test file

---
+++ test_file.py
@@ -0,0 +1,47 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression as SLR
+from logistic_regression import LogisticRegression as LR
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_digits
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+#v = load_digits()
+    
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        _dict = pickle.load(fo, encoding='bytes')
+    return _dict
+
+_X_train = None
+_y_train = None
+X_train = np.zeros([50000, 32, 32, 3], dtype=int)
+
+def convToImg(imgArr):
+    return imgArr.reshape(3, -1).T.reshape(32, 32, 3)
+
+for i in range(1, 6):
+    i_dict = unpickle("/Users/aihya/goinfre/cifar-10-batches-py/data_batch_{}".format(str(i)))
+    if X_train is None:
+        X_train = i_dict[b'data']
+    else:
+        X_train = np.concatenate((X_train, i_dict[b'data']), axis=0)
+    if y_train is None:
+        y_train = i_dict[b'labels']
+    else:
+        y_train = np.concatenate((y_train, i_dict[b'labels']))
+
+for imgArr, idx in enumerate(_X_train):
+    if X_train[idx] is None:
+
+
+
+#X_train, X_test, y_train, y_test = train_test_split(v.data, v.target,
+#        test_size=0.2)
+
+lr = LR(normalize=False)
+slr = SLR(multi_class='ovr')
+
