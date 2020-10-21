778d4a09cc09cbe38a14b0c37651b2b46c8abdcf
aihya <aihya@student.1337.ma>
2020-10-21 14:31:05 +0100

    new test file

--- test_file.py
+++ test_file.py
@@ -9,21 +9,22 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-#v = load_digits()
-    
+v = load_digits()
+   
 def unpickle(file):
     with open(file, 'rb') as fo:
         _dict = pickle.load(fo, encoding='bytes')
     return _dict
 
-_X_train = None
-_y_train = None
-X_train = np.zeros([50000, 32, 32, 3], dtype=int)
+X_train = None
+y_train = None
+#X_train = np.zeros([50000, 32, 32], dtype=int)
 
 def convToImg(imgArr):
-    return imgArr.reshape(3, -1).T.reshape(32, 32, 3)
+    rgb_weights = [0.2989, 0.5870, 0.1140]
+    return np.dot(imgArr.reshape(3, -1).T.reshape(32, 32, 3), rgb_weights)
 
-for i in range(1, 6):
+for i in range(1, 2):
     i_dict = unpickle("/Users/aihya/goinfre/cifar-10-batches-py/data_batch_{}".format(str(i)))
     if X_train is None:
         X_train = i_dict[b'data']
@@ -33,15 +34,18 @@ for i in range(1, 6):
         y_train = i_dict[b'labels']
     else:
         y_train = np.concatenate((y_train, i_dict[b'labels']))
+"""
+print("Converting...")
+for idx, imgArr in enumerate(_X_train):
+    print(idx)
+    X_train[idx] = convToImg(imgArr)
 
-for imgArr, idx in enumerate(_X_train):
-    if X_train[idx] is None:
-
-
-
+print("Done converting.")
+"""
 #X_train, X_test, y_train, y_test = train_test_split(v.data, v.target,
 #        test_size=0.2)
 
+#X_train = np.array(X_train, dtype=np.float128)
 lr = LR(normalize=False)
 slr = SLR(multi_class='ovr')
 
